4a00749c9ef8955d222d2bbce5aca259f688b493
Reuben Fischer-Baum <reuben.fischerbaum@gmail.com>
2018-03-01 22:17:09 -0500

    tweaks

--- src/js/app.js
+++ src/js/app.js
@@ -7,14 +7,14 @@ const oneDay = 24*60*60*1000;
 let today = new Date();
 let electionDay = new Date(2018,10,6);
 let diffDays = Math.round(Math.abs((today.getTime() - electionDay.getTime())/(oneDay)));
+	diffDays == diffDays < 0 ? 0 : diffDays;
 let dayText1 = diffDays == 1 ? 'is' : 'are';
 let dayText2 = diffDays == 1 ? 'day' : 'days';
 
 d3.select('.text')
-	.html(`<p>There ${dayText1}</p><p class = 'days'>${diffDays} ${dayText2}</p><p>until election day 2018</p>`);
-
-d3.select('.days')
-	.style('font-size', `${249 - diffDays + 7}px`);
+	.html(`<p>There ${dayText1}</p><p class = 'days'>${diffDays} ${dayText2}</p><p>until election day</p>`)
+	.style('font-size', `${249 - diffDays + 6}px`);
+	
 
 
 
