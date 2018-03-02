a23440699ac31fa6090cf001758a44227dbf2231
Reuben Fischer-Baum <reuben.fischerbaum@gmail.com>
2018-03-02 08:26:07 -0500

    Fix date

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,11 @@ const d3 = require('d3-selection');
 
 const oneDay = 24*60*60*1000;
 
-let today = new Date();
-let electionDay = new Date(2018,10,6);
-let diffDays = Math.round(Math.abs((today.getTime() - electionDay.getTime())/(oneDay)));
+let today = new Date().setHours(0,0,0,0);
+let electionDay = new Date(2018,10,6).getTime();
+
+
+let diffDays =(electionDay - today)/(oneDay);
 	diffDays == diffDays < 0 ? 0 : diffDays;
 let dayText1 = diffDays == 1 ? 'is' : 'are';
 let dayText2 = diffDays == 1 ? 'day' : 'days';
