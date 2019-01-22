07a2722dfa6dedfb718c59ee8c468cc0eb3d87aa
Reuben Fischer-Baum <reuben.fischerbaum@gmail.com>
2019-01-22 13:36:07 -0500

    Point at 2020

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@ const d3 = require('d3-selection');
 const oneDay = 24*60*60*1000;
 
 let today = new Date().setHours(0,0,0,0);
-let electionDay = new Date(2018,10,6).getTime();
+let electionDay = new Date(2020,10,3).getTime();
 
 
 let diffDays = Math.round((electionDay - today)/(oneDay));
@@ -15,7 +15,7 @@ let dayText2 = diffDays == 1 ? 'day' : 'days';
 
 d3.select('.text')
 	.html(`<p>There ${dayText1}</p><p class = 'days'>${diffDays} ${dayText2}</p><p>until election day</p>`)
-	.style('font-size', `${249 - diffDays + 6}px`);
+	.style('font-size', `${651 - diffDays + 6}px`);
 	
 
 
